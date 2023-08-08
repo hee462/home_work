2a47ebba3229afae1bc50f09d89cc1e3e3388d5f
heath <byhuisun@icloud.com>
2023-08-08 17:37:20 +0900

    movie

---
+++ react/movie/src/comps/Main.js
@@ -0,0 +1,13 @@
+import "../css/Main.css"
+import Input from "./input"
+
+const Main =()=>{
+    return(
+        <>
+        <h1>영화 보세요</h1>
+        <Input/>
+        
+        </>
+    )
+}
+export default Main;
